79259e0871c428067cacdff45f135ce652e8ab31
Luidgi Sarto Lacerda <luidgi.lacerda@gmail.com>
2019-06-18 08:24:19 -0300

    Ajustes na home

---
+++ src/main/java/br/ufjf/luidgisarto/trb2/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.ufjf.luidgisarto.trb2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({ "/", "/index.html" })
+    public String index() {
+        return "index";
+    }
+}
