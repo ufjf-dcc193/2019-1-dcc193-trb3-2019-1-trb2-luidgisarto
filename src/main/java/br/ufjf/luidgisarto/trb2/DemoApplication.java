30f3ea8c04a2a5c15dda0c68a01f5ad0f0326bae
Luidgi Sarto Lacerda <luidgi.lacerda@gmail.com>
2019-06-18 08:22:42 -0300

    Criação das áreas iniciais do sistema no banco

--- src/main/java/br/ufjf/luidgisarto/trb2/DemoApplication.java
+++ src/main/java/br/ufjf/luidgisarto/trb2/DemoApplication.java
@@ -1,13 +1,27 @@
 package br.ufjf.luidgisarto.trb2;
 
+import br.ufjf.luidgisarto.trb2.models.Area;
+import br.ufjf.luidgisarto.trb2.repositories.AreaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+		AreaRepository areaRepository = ctx.getBean(AreaRepository.class);
+
+		areaRepository.deleteAll();
+
+		for (int i = 1; i < 6; i++) {
+			Area a = new Area("Area " + i);
+
+			areaRepository.save(a);
+		}
 	}
 
 }
